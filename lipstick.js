2ad1c3eb6d50d60c787c579efdbfb94d16469322
mohit <75508470+Mohit-Coder99@users.noreply.github.com>
2021-05-17 20:24:23 -0400

    Add files via upload

--- lipstick.js
+++ lipstick.js
@@ -4,11 +4,20 @@ function preload(){
 function setup(){
      canvas = createCanvas(300, 300);
      canvas.center();
+     video = createCapture(VIDEO);
+     video.size(400,300);
+     video.hide();
+     poseNet = ml5.poseNet(video, modelLoaded);
+     poseNet.on('pose', gotPoses);
 }
 
+image(video, 0, 0, 400, 300);
+
+
 function draw(){
+     image(video, 0, 0, 400, 300);
 }
 
 function take_snapshot(){
-     save('myfilterimage.png');
+     save('myLipstick.png');
 }
